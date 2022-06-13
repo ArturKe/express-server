38af4e862d36abc0551910ceebd67ee1f5896e39
Artur <vashpartner@gmail.com>
2022-06-13 10:29:33 +0300

    Add auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,62 @@
 const db = require('../db')
+// const {validationResult} = require('express-validator')
 class UserController {
+  async registerUser (req,res) {
+    try {
+      //Validation
+      // const validationErrors = validationResult(req)
+      // if(!validationErrors.isEmpty()) {
+      //   return res.status(400).send('Registration error!' + validationErrors)
+      // }
+      //Проверка на существование пользователя, проверка по логину
+      const candidate = await db.query(`SELECT name from USERS where name=`)
+      if (candidate) {
+        return res.send('Такой пользователь уже существует')
+      }
+      //Хешируем пароль
+      // const hashPassword = bcrypt.hashSync(password, 7)
+      // const role = await db.query(`SELECT rule from RULES where rule="regularUser"`) Берем из БД нужную роль
+      // Сохраняем в БД логин, роль и захешированный пароль
+    } catch (error) {
+      
+    }
+  }
+
+  async loginUser (req,res) {
+    try {
+      // Достаем данные пользователя
+      const {username, password} = req.body
+      // Ищем пользователя с таким именем в БД
+      const user = await db.query(`SELECT name from USERS where name=`, [username])
+      if (!user) {
+        return res.send('Такой пользователь не найден.')
+      }
+      // Сравниваем пароли
+      // const validPassword = bcrypt.compareSync(password, userPassword)
+      if (!validPassword) {
+        return res.send('Пароль не верный')
+      }
+    } catch (error) {
+      
+    }
+
+  }
+
   async createUser (req,res) {
 
   }
 
   async getUsers (req, res) {
-    const userInfo = await db.query(`SELECT * from users`)
-    res.send(userInfo.rows)
+    try {
+      const userInfo = await db.query(`SELECT * from users`)
+      res.send(userInfo.rows)
+    } catch (error) {
+      res.status(400).send('Page not found!')
+    }
   }
 
   async getOneUser (req,res) {
-    res.send('ALL USERS HERE!')
+    res.send('ONE USERS HERE!' + req)
   }
 
   async updateUser (req,res) {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 const Router = require('express')
 const userController = require('../controllers/user.controller')
 const router = new Router
+// const {check} = require('express-validator')
 
+router.post('/api/users', [
+  // check('username', 'ИМя пользователя не может быть пустым').notEmpty()
+  // check('password', 'Пароль не может быть короче 4 и длиннее 10 символов').isLength({min:4, max:10})
+], userController.registerUser)
 router.post('/user', userController.createUser)
 router.get('/user', userController.getUsers)
 // router.get('/user',(req, res) => {
